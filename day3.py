65e5280e19e742c33ad05c8daab157807db6bcb0
Torok, Daniel Janos <daniel.torok@sap.com>
2017-12-08 18:23:10 +0100

    [Day 3] part 2

--- day3.py
+++ day3.py
@@ -1,6 +1,6 @@
 input = 347991
-input = 347991
 
+# part 1
 sector = 0
 square = 1
 while square**2 < input:
@@ -9,15 +9,47 @@ while square**2 < input:
 
 max = square**2
 target = max - square + 1
-print(target, max, square)
 
 while target > input:
     target -= (square - 1)
     max -= (square - 1)
-print(target, "...", input, "...", max)
 
 halfpoint = (max + target) // 2
-print(halfpoint)
-
 result = abs(input - halfpoint) + sector
-print(result)
+print("Part 1 result:", result)
+
+# part 2
+from operator import add
+
+matrix = {(0, 0): 1, (0, 1): 1}
+# right, up, left, down
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+# want to go left
+currentDirectionIndex = 1
+currentPosition = (0, 1)
+
+def addTuple(x, y):
+    return tuple(map(add, x, y))
+
+xs = [-1, 0, 1] * 3
+ys = [-1] * 3 + [0] * 3 + [1] * 3
+ranges = list(zip(xs, ys))
+
+while True:
+    currentDirection = directions[currentDirectionIndex]
+    nextDirection = directions[(currentDirectionIndex + 1) % 4]
+
+    if addTuple(currentPosition, nextDirection) not in matrix:
+        currentDirectionIndex = (currentDirectionIndex + 1) % 4
+        currentDirection = nextDirection
+    currentPosition = addTuple(currentPosition, currentDirection)
+
+    value = 0
+    for coordinate in ranges:
+        index = addTuple(currentPosition, coordinate)
+        value += matrix.get(index, 0)
+    matrix[currentPosition] = value
+
+    if value > input:
+        print("Part 2 result:", value)
+        break
